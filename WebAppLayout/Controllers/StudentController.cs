8e45a2695291866c098b05dd3408a35826ed7dc2
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-01 16:46:11 +0530

    Add project files.

---
+++ WebAppLayout/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppLayout.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.msg = "Student Home Page";
+            return View();
+        }
+        public ActionResult NameList() {
+        List<string> list = new List<string>() { 
+        "Naveen","Jon","Sam","Leo","Fiz"
+        };
+            ViewBag.list = list;
+            return View();
+        }
+    }
+}
